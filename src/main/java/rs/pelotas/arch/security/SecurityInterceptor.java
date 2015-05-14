19255657381dbdf5852569f478bd86d59797268f
Rafael Guterres <guterresrafael@gmail.com>
2015-05-14 01:21:06 -0300

    Refactor no interceptor que realiza a autenticacao e autorizacao do usuario.

--- src/main/java/rs/pelotas/arch/security/SecurityInterceptor.java
+++ src/main/java/rs/pelotas/arch/security/SecurityInterceptor.java
@@ -24,17 +24,18 @@ import org.jboss.resteasy.util.Base64;
  * @author Rafael Guterres
  */
 @Provider
-public class JBossSecurityInterceptor implements ContainerRequestFilter {
+public abstract class SecurityInterceptor implements ContainerRequestFilter {
 
+    private static final String RESOURCE_METHOD_INVOKER_CLASS = "org.jboss.resteasy.core.ResourceMethodInvoker";
     private static final String AUTHORIZATION_PROPERTY = "Authorization";
     private static final String AUTHENTICATION_SCHEME = "Basic";
-    private static final ServerResponse ACCESS_DENIED = new ServerResponse(null, 401, new Headers<>());
-    private static final ServerResponse ACCESS_FORBIDDEN = new ServerResponse(null, 403, new Headers<>());
-    private static final ServerResponse SERVER_ERROR = new ServerResponse(null, 500, new Headers<>());
+    private static final ServerResponse HTTP_401_UNAUTHORIZED = new ServerResponse(null, 401, new Headers<>());
+    private static final ServerResponse HTTP_403_FORBIDDEN = new ServerResponse(null, 403, new Headers<>());
+    private static final ServerResponse HTTP_500_INTERNAL_SERVER_ERROR = new ServerResponse(null, 500, new Headers<>());
 
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
-        ResourceMethodInvoker methodInvoker = (ResourceMethodInvoker) requestContext.getProperty("org.jboss.resteasy.core.ResourceMethodInvoker");
+        ResourceMethodInvoker methodInvoker = (ResourceMethodInvoker) requestContext.getProperty(RESOURCE_METHOD_INVOKER_CLASS);
         Method method = methodInvoker.getMethod();
 
         //Access allowed for all
@@ -42,70 +43,54 @@ public class JBossSecurityInterceptor implements ContainerRequestFilter {
         
             //Access denied for all
             if (method.isAnnotationPresent(DenyAll.class)) {
-                requestContext.abortWith(ACCESS_FORBIDDEN);
+                requestContext.abortWith(HTTP_403_FORBIDDEN);
                 return;
             }
 
-            //Get request headers
-            final MultivaluedMap<String, String> headers = requestContext.getHeaders();
-
-            //Fetch authorization header
-            final List<String> authorization = headers.get(AUTHORIZATION_PROPERTY);
-
+            //Get request headers and fetch authorization header.
             //If no authorization information present; block access
+            final MultivaluedMap<String, String> headers = requestContext.getHeaders();
+            final List<String> authorization = headers.get(AUTHORIZATION_PROPERTY);
             if (authorization == null || authorization.isEmpty()) {
-                requestContext.abortWith(ACCESS_DENIED);
+                requestContext.abortWith(HTTP_401_UNAUTHORIZED);
                 return;
             }
 
             //Get encoded username and password
-            final String encodedUserPassword = authorization.get(0).replaceFirst(AUTHENTICATION_SCHEME + " ", "");
-
             //Decode username and password
-            String usernameAndPassword = null;
+            //Split username and password tokens
+            final String encodedUserPassword = authorization.get(0).replaceFirst(AUTHENTICATION_SCHEME + " ", "");
+            String usernameAndPassword;
             try {
                 usernameAndPassword = new String(Base64.decode(encodedUserPassword));
             } catch (IOException e) {
-                requestContext.abortWith(SERVER_ERROR);
+                requestContext.abortWith(HTTP_500_INTERNAL_SERVER_ERROR);
                 return;
             }
-
-            //Split username and password tokens
             final StringTokenizer tokenizer = new StringTokenizer(usernameAndPassword, ":");
             final String username = tokenizer.nextToken();
             final String password = tokenizer.nextToken();
 
-            //Verifying Username and password
-            System.out.println(username);
-            System.out.println(password);
-
+            //Verify user password
+            if (!isValidPassword(username, password)) {
+                requestContext.abortWith(HTTP_401_UNAUTHORIZED);
+                return;
+            }
+            
             //Verify user access
             if (method.isAnnotationPresent(RolesAllowed.class)) {
                 RolesAllowed rolesAnnotation = method.getAnnotation(RolesAllowed.class);
-                Set<String> rolesSet = new HashSet<>(Arrays.asList(rolesAnnotation.value()));
-
-                //Is user valid?
-                if (!isUserAllowed(username, password, rolesSet)) {
-                    requestContext.abortWith(ACCESS_DENIED);
-                    return;
+                Set<String> roles = new HashSet<>(Arrays.asList(rolesAnnotation.value()));
+                
+                //Is user allowed?
+                if (!isAllowedUser(username, roles)) {
+                    requestContext.abortWith(HTTP_403_FORBIDDEN);
                 }
             }
         }
     }
 
-    private boolean isUserAllowed(final String username, final String password, final Set<String> rolesSet) {
-        boolean isAllowed = false;
-
-        //Step 1. Fetch password from database and match with password in argument
-        //If both match then get the defined role for user from database and continue; else return isAllowed [false]
-        //Access the database and do this part yourself
-        //String userRole = userMgr.getUserRole(username);
-        String userRole = "ADMIN";
-
-        //Step 2. Verify user role
-        if (rolesSet.contains(userRole)) {
-            isAllowed = true;
-        }
-        return isAllowed;
-    }
+    public abstract boolean isValidPassword(final String username, final String password);
+    
+    public abstract boolean isAllowedUser(final String username, final Set<String> roles);
 }
